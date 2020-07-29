b33177a328c0a454755e5da428af982bde3b639d
Luca Isidoro <apprendista44@gmail.com>
2020-07-29 15:52:19 +0200

    commit con preparazione e organizzazione ambiente

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <title>@yield('title')</title>
+  </head>
+  <body>
+    @include('partials.header')
+    @yield('section')
+    @include('partials.footer')
+  </body>
+</html>
